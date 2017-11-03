91f3fbb5c36183a4e9710fe342522e595f1b0c18
dhemzal <33301834+dhemzal@users.noreply.github.com>
2017-11-03 09:34:27 +0100

    Add files via upload

--- basic/cummulation.pl
+++ basic/cummulation.pl
@@ -1,13 +1,25 @@
 #!/usr/bin/perl
 
 
-open(INIT,"cummulation.ini") or die;
-while ($radek=<INIT>){
-chomp($radek);
+# perl cummulation.pl -i inifile -v outfile file1,..,fileN weigh1,..,weighN
+# ver 1.0
 
-if (($radek !~/^\%/) and ($radek ne "")){
-  @pomo=split(/#/,$radek);
-  $ini{$pomo[0]}=$pomo[1];
+
+
+
+use GetOpt::Std;
+getopts('vi:');
+
+$topen="cummulation.ini";
+if ($opt_i ne ""){$topen=$opt_i.".ini"};
+
+open(INIT,$topen) or die;
+while ($rrow=<INIT>){
+  chomp($rrow);
+
+  if (($rrow !~/^\%/) and ($rrow ne "")){
+    @pomo=split(/#/,$rrow);
+    $ini{$pomo[0]}=$pomo[1];
   }
 }
 close INIT;
@@ -18,53 +30,48 @@ close INIT;
 
 
 
-@znormovat=split(/,/,$ARGV[1]);
-@vahy=split(/,/,$ARGV[2]);
+@tonorm=split(/,/,$ARGV[1]);
+@weighs=split(/,/,$ARGV[2]);
 
 
-$preskok=0;
-$soubor="$ini{prefix}"."$znormovat[0]"."$ini{suffix}";
-open(VYSTUP, "$soubor") or $preskok=1;
-
-
-
-
-print "$ARGV[0]:\n";
-
-
-for ($cykl=$preskok;$cykl<@znormovat;$cykl++){
-
-print " $vahy[$cykl]x $znormovat[$cykl]\n";
-
-$soubor="$ini{prefix}"."$znormovat[$cykl]"."$ini{suffix}";
-open(VSTUP,"$soubor") or die;
-
-$i=0;
-while ($radek=<VSTUP>){
-chomp($radek);($x[$i],$y[$i])=split(/\t/,$radek);
-if ($cykl == 0){$kumul[$i]=0}
-$kumul[$i]+=$y[$i]*$vahy[$cykl];
-$i++
-}
-
-$imax=$i;
-
-close(VSTUP);
-
-
+$skip=0;
+if ($ARGV[0]==$tonorm[0]){
+  $datfile="$ini{prefix}"."$tonorm[0]"."$ini{suffix}";
+  unless (-f $datfile){$skip=1};
 }
 
 
 
+$opt_v and print "$ARGV[0]:\n";
 
-$soubor="$ini{prefix}"."$ARGV[0]"."$ini{suffix}";
-open(VYSTUP, ">$soubor") or $preskok=1;
+
+for ($cycl=$skip;$cycl<@tonorm;$cycl++){
+
+$opt_v and print " $weighs[$cycl]x $tonorm[$cycl]\n";
+
+  $datfile="$ini{prefix}"."$tonorm[$cycl]"."$ini{suffix}";
+  open(INPUT,"$datfile") or die;
+
+  $i=0;
+  while ($rrow=<INPUT>){
+    chomp($rrow);($x[$i],$y[$i])=split(/\t/,$rrow);
+    if ($cycl == 0){$cummul[$i]=0}
+    $cummul[$i]+=$y[$i]*$weighs[$cycl];
+    $i++
+  }
+  $imax=$i;
+
+  close(INPUT);
+}
+
+$datfile="$ini{prefix}"."$ARGV[0]"."$ini{suffix}";
+open(OUTPUT, ">$datfile") or die;
 
 for ($i=0;$i<$imax;$i++){
-  print VYSTUP "$x[$i]\t$kumul[$i]\n";
+  print OUTPUT "$x[$i]\t$cummul[$i]\n";
 }
 
-close (VYSTUP);
+close (OUTPUT);
 
 
 
